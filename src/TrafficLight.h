838bc4d4faae75755fff3a7752564e48588c5deb
Peter de Boer <peterdeboer_private@hotmail.com>
2021-01-17 12:19:27 +0000

    define class MessageQueue

--- src/TrafficLight.h
+++ src/TrafficLight.h
@@ -19,10 +19,15 @@ template <class T>
 class MessageQueue
 {
 public:
-
+    T receive();
+    void send(T &&msg);
+   
 private:
-    
+    std::mutex _mutex;
+    std::condition_variable _cond;
+    std::deque<T> _messages;
 };
+
 
 // FP.1 : Define a class „TrafficLight“ which is a child class of TrafficObject. 
 // The class shall have the public methods „void waitForGreen()“ and „void simulate()“ 
