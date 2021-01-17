36c2a4a3955305a46e74a434b66d83a31b976782
Peter de Boer <peterdeboer_private@hotmail.com>
2021-01-17 09:36:35 +0000

    implement TrafficLight::cycleThroughPhases and simulate

--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -41,9 +41,12 @@ TrafficLightPhase TrafficLight::getCurrentPhase()
     return _currentPhase;
 }
 
+*/
+
 void TrafficLight::simulate()
 {
     // FP.2b : Finally, the private method „cycleThroughPhases“ should be started in a thread when the public method „simulate“ is called. To do this, use the thread queue in the base class. 
+    threads.emplace_back(std::thread(&TrafficLight::cycleThroughPhases, this));
 }
 
 // virtual function which is executed in a thread
@@ -52,7 +55,26 @@ void TrafficLight::cycleThroughPhases()
     // FP.2a : Implement the function with an infinite loop that measures the time between two loop cycles 
     // and toggles the current phase of the traffic light between red and green and sends an update method 
     // to the message queue using move semantics. The cycle duration should be a random value between 4 and 6 seconds. 
-    // Also, the while-loop should use std::this_thread::sleep_for to wait 1ms between two cycles. 
+    // Also, the while-loop should use std::this_thread::sleep_for to wait 1ms between two cycles.
+    std::random_device rd;  //Will be used to obtain a seed for the random number engine
+    std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
+    std::uniform_real_distribution<float> dis(4.0, 6.0);
+    while (true) {
+      double cycleDuration = dis(gen); // duration of a single phase in s
+      std::chrono::time_point<std::chrono::system_clock> lastUpdate;
+      // init stop watch
+      lastUpdate = std::chrono::system_clock::now();
+      long timeSinceLastUpdate;
+      do {
+        // sleep at every iteration to reduce CPU usage
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+
+        // compute time difference to stop watch
+        timeSinceLastUpdate = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - lastUpdate).count();     
+      }
+      while (timeSinceLastUpdate / 1000.0 < cycleDuration);
+      _currentPhase = (_currentPhase == green) ? red : green; 
+      _condition.notify_one();
+    }
 }
 
-*/
--- src/TrafficLight.h
+++ src/TrafficLight.h
@@ -30,6 +30,8 @@ private:
 // can be either „red“ or „green“. Also, add the private method „void cycleThroughPhases()“. 
 // Furthermore, there shall be the private member _currentPhase which can take „red“ or „green“ as its value. 
 
+enum TrafficLightPhase {red, green};
+
 class TrafficLight: public TrafficObject
 {
 public:
@@ -39,20 +41,19 @@ public:
 
     // typical behaviour methods
     void waitForGreen();
-    void cycleThroughPhases();
-    enum TrafficLightPhase {red, green};
+    void simulate();  
     TrafficLightPhase getCurrentPhase();
 
 private:
     // typical behaviour methods
-    void cycleThroughPhase();
+    void cycleThroughPhases();
 
     // FP.4b : create a private member of type MessageQueue for messages of type TrafficLightPhase 
     // and use it within the infinite loop to push each new TrafficLightPhase into it by calling 
     // send in conjunction with move semantics.
 
     std::condition_variable _condition;
-    std::mutex _mutex;
+    std::mutex _mutex; // TODO: mutex on _currentPhase
     TrafficLightPhase _currentPhase;
 };
 
